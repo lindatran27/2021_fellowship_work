3837922e448cd206326cc8e73bdeb64c8010647c
lindatran27 <lindatran432@gmail.com>
2021-07-07 01:01:55 -0400

    updated

---
+++ week_4_guided_inquiry/week_4_hard.py
@@ -0,0 +1,6 @@
+# HARD: Write a function that takes in an array of numbers and outputs the average of all the numbers. 
+numbers = [1, 2, 3, 4, 5]
+
+average = sum(numbers) / len(numbers)
+
+print(average)
