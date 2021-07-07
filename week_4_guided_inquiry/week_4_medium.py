343aea3a3a4339e08bbb7cc6486cecb8ab6fd1e6
lindatran27 <lindatran432@gmail.com>
2021-07-07 00:47:19 -0400

    updated files

--- week_4_guided_inquiry/week_4_medium.py
+++ week_4_guided_inquiry/week_4_medium.py
@@ -1,6 +1,8 @@
 # MEDIUM: Create a program that accepts a number (1-12) as input and logs to the console that number and its corresponding month. 
 # For example: if the user enters the number 3, then it should return the month “March.” 
 # Alert the user if they enter an invalid number (e.g. less than 1 or greater than 12).
+
+
 number = input("input a number: ")
 
 if int(number) == int(1):
@@ -27,7 +29,7 @@ elif int(number) == int(11):
     print("november")
 elif int(number) == int(12):
     print("december")
-elif int(number) => int(1-12):
-    print("not within 1-12")
+else:
+    print("not in range")
 
     
